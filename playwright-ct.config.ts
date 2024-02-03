df7260a15bb3cfefc1d24d5b30a8e0d2aafeb5ce
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2024-02-03 12:17:18 +0000

    Corrected testDir config to improve report aggregation

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -5,7 +5,7 @@ import { SerenityOptions } from '@serenity-js/playwright-test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig<SerenityOptions>({
-    testDir: './',
+    testDir: './src',
     /* The base directory, relative to the config file, for snapshot files created with toMatchSnapshot and toHaveScreenshot. */
     snapshotDir: './__snapshots__',
     /* Maximum time one test can run for. */
